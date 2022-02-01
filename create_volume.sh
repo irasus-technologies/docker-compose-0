eceda4296c9afe8415dc3fb15c4a238d87f75ef4
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2022-02-01 14:53:16 +0530

    Create external named volume for Node-RED data persistence

--- create_volume.sh
+++ create_volume.sh
@@ -1,2 +1,3 @@
 docker volume create -d local-persist -o mountpoint=/home/anirudh/Data/Docker/mosquitto --name=mosquitto
+docker volume create -d local-persist -o mountpoint=/home/anirudh/Data/Docker/node-red --name=node-red
 
