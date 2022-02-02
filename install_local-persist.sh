56a93fd7d75f1cf2142ca9109072ca55ed431b36
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2022-02-02 20:49:37 +0530

    Add installer for Docker local-persist

---
+++ install_local-persist.sh
@@ -0,0 +1,2 @@
+# curl -fsSL https://raw.githubusercontent.com/MatchbookLab/local-persist/master/scripts/install.sh | sudo bash
+curl -fsSL https://raw.githubusercontent.com/anirudh-ramesh/local-persist/master/scripts/install.sh | sudo bash
